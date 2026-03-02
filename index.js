2408fcedee27c6b46fce180cda1e7f86de53e72e
3luko <ethanlukandwa@gmail.com>
2026-03-02 16:04:42 -0600

    random push

---
+++ index.js
@@ -0,0 +1,25 @@
+const button = document.querySelector("button");
+button.addEventListener("click", () => alert("Clicked!"))
+
+
+// function myFunc(a, b, c){
+//     function otherFunc(a, b){
+//         a += 1;
+//         return a + b;
+//     }
+//     var d = otherFunc(a, b);
+//     console.log(a, b, c, d);
+// }
+
+// myFunc(1, 3.14, "hi");
+// myFunc(1, "hi");
+// otherFunc(5, 8);
+
+function printBox(numRows, numCols, boxChar = "X"){
+    for(i = 0; i < numRows; i++){
+        
+
+    }
+    
+
+}
